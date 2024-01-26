db2679911f8438e2e638a679004812d8ba2e2199
marya479 <m.weglinska479@gmail.com>
2024-01-26 10:03:25 +0100

    fixed

--- src/index.js
+++ src/index.js
@@ -52,6 +52,13 @@ function formatDate(date) {
   let formattedDay = days[day];
   return `${formattedDay} ${hours}:${minutes}`;
 }
+
+function getForecast(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
 function displayForecast(response) {
   let forecastHtml = "";
 
